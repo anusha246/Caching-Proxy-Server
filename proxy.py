e3cd46d2097d09854f23d55d8958677447697ffa
pahorean <anusha.pahore@mail.utoronto.ca>
2021-01-27 18:56:15 -0500

    Cache responses as files

--- proxy.py
+++ proxy.py
@@ -29,7 +29,6 @@ while True:
         header = connection.recv(1000)
         print('received {!r}'.format(header))
         if header:
-            print('sending data back to the client')
 
             first_line = header.split(b'\n')[0]
             url = first_line.split(b' ')[1]
@@ -40,72 +39,105 @@ while True:
             for i in range(2, len(url.split(b'/'))):
                 relative_url += b'/' + url.split(b'/')[i]
 
-            
+            if relative_url == b'':
+                relative_url = b'/'
+
+        
+            #Try sending cached response, if it exists    
             try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                filename = host.decode('utf-8') + ' ' + \
+                           relative_url.decode('utf-8').replace('/', ',')
+                print(filename)
+                file = open(filename + ".bin", mode='rb')
+                connection.sendall(file.read())
+                print('File read successful')
 
-                print(host)
-                print(host.decode('utf-8'))
-                s.connect((host.decode('utf-8'), 80))
+            except: #File failed to open, may not exist
+                try: #Try connecting to web server
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-                if relative_url == b'':
-                    relative_url = b'/'
+                    print(host)
+                    print(host.decode('utf-8'))
+                    s.connect((host.decode('utf-8'), 80))
 
-                header = header.replace(url, relative_url)
-                header = header.replace(b'localhost:8888', host)
-                
-                s.sendall(header)
-
-                print('Header sent successfully')
-
-                response = s.recv(65565)
-
-                response_header = response.split(b'\r\n\r\n')[0]
-
-                print(response)
-
-                try:
-                    byte_content_length = response_header.split(b'Content-Length: ')[1].split(b'\r\n')[0]
-                    content_length = int(byte_content_length.decode('utf-8'))
-
-                    content = response.split(b'\r\n\r\n')[1]
-                    while len(content) != content_length:
-                        print('Content length: {}, needed len: {}'.format(len(content), content_length))
-                        content += s.recv(65565)
-
-                    print(response_header + b'\r\n\r\n' + content)
-
-                    connection.sendall(response_header + b'\r\n\r\n' + content)
-                    print('Sent response to client')
-                except: #HTTP code 304 or Content-Length not specified
-                    print('HTTP code 304 or Content-Length not specified')
-
-                    if b'200 OK' in response:
-                        while len(response) > 0:
-                            print (response)
-                            connection.send(response)
-                            print('Sent response to client')
-                            response = s.recv(1024)
-                            print('Receieved response from web server')
-                    else:
-                        connection.send(response)
+                    header = header.replace(url, relative_url)
+                    header = header.replace(b'localhost:8888', host)
+                    print(header)
                     
-                finally:
-                    s.close()
+                    s.sendall(header)
+                    print('Header sent successfully')
+                    
 
-            
-            #Except block below from
-            #https://stackoverflow.com/questions/32792333/
-            #python-socket-module-connecting-to-an-http-proxy-then-performing-a-get-request
-            except socket.error as m:
-               print (str(m))
-               s.close()
-               sys.exit(1)            
+                    response = s.recv(65565)
+
+                    response_header = response.split(b'\r\n\r\n')[0]
+
+                    print(response)
+
+                    try:
+                        byte_content_length = response_header.split(b'Content-Length: ')[1].split(b'\r\n')[0]
+                        content_length = int(byte_content_length.decode('utf-8'))
+
+                        content = response.split(b'\r\n\r\n')[1]
+                        while len(content) != content_length:
+                            print('Content length: {}, needed len: {}'.format(len(content), content_length))
+                            content += s.recv(65565)
+
+                        response = response_header + b'\r\n\r\n' + content #this is the full response now
+
+                        print(response)
+
+                        connection.sendall(response)
+                        print('Sent response to client')
+                        
+                    except: #HTTP code 304 or Content-Length not specified
+                        print('HTTP code 304 or Content-Length not specified')
+
+                        if b'200 OK' in response:
+
+                            while True:
+                                incoming = s.recv(65565)
+                                if len(incoming) == 0:
+                                    break;
+                                print (incoming)
+                                print('Receieved incoming data from web server')
+                                response += incoming
+
+                        connection.sendall(response)
+                        
+                    finally:
+
+                        #Write the response to file
+                        try:
+                            filename = host.decode('utf-8') + ' ' + \
+                                       relative_url.decode('utf-8').replace('/', ',')
+                            file = open(filename + ".bin", mode='wb')
+                            print(response)
+                            file.write(response)
+                            
+                            print('Response written successfully')
+                        finally:
+                            file.close()
+
+                            
+                        s.close()
+
+                
+                #Except block below from
+                #https://stackoverflow.com/questions/32792333/
+                #python-socket-module-connecting-to-an-http-proxy-then-performing-a-get-request
+                except socket.error as m:
+                   print (str(m))
+                   s.close()
+                   sys.exit(1)
+
+            finally:
+                file.close()
             
             
         else:
             print('no data from', client_address)
-            break
+            #break
 
     finally:
         # Clean up the connection
