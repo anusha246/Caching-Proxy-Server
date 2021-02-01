2f5c2f2b8ecce4a5f52b4a69d5983996f1b49378
pahorean <anusha.pahore@mail.utoronto.ca>
2021-02-01 01:11:01 -0500

    Fix unupdated Content-Length cache bug

--- proxy.py
+++ proxy.py
@@ -3,6 +3,15 @@ Code inspiration and snippets from:
 https://pymotw.com/3/socket/tcp.html
 https://stackoverflow.com/questions/32792333/
     python-socket-module-connecting-to-an-http-proxy-then-performing-a-get-request
+
+Steps 1, 3, 4 and 5 have been completed, however step 2 has not.
+It was never able to be fully implemented because of confusion 
+around how the line 'readable, writable, exceptional = 
+select.select(inputs, outputs, inputs)' was operating. It appeared to
+arbitrarily add sockets to writable as well as add multiple sockets to
+writable even when there is only a single socket that was having information
+written to it. Due to this step 2 was left unimplemented and we moved our 
+attention to later steps.
 '''
 
 import sys, os, time, socket, select
@@ -20,7 +29,7 @@ def proxy(expiry):
 
     # Bind the socket to the port
     server_address = ('localhost', 8888)
-    print('starting up on {} port {}'.format(*server_address))
+    
     sock.bind(server_address)
 
     # Listen for incoming connections
@@ -28,15 +37,12 @@ def proxy(expiry):
 
     while True:
         # Wait for a connection
-        print('waiting for a connection')
+        
         connection, client_address = sock.accept()
         try:
-            print('connection from', client_address)
-            
             header = connection.recv(1000)
-            print('received {!r}'.format(header))
+            
             if header:
-
                 first_line = header.split(b'\n')[0]
                 url = first_line.split(b' ')[1]
                 
@@ -52,41 +58,32 @@ def proxy(expiry):
                 filename = host.decode('utf-8') + ' ' \
                            + relative_url.decode('utf-8').replace('/', ',') \
                            + '.bin'
-                print('Filename: {}'.format(filename))
 
                 is_expired = True
             
                 #Check if a file is expired, if it exists
                 try:
                     is_expired = time.time() - os.path.getmtime(filename) >= expiry
-                    print('Elapsed time is {}, expiry is {}'.format(\
-                            time.time() - os.path.getmtime(filename), expiry))
+                    
                 except:
-                    print('')
-                    print('File does not exist')
+                    pass #File does not exist
 
                 if not is_expired:
                     with open(filename, mode='rb') as file:
                         connection.sendall(file.read())
-                    print('File read successful')
-                    
+  
                 else:    
                     try: #Try connecting to web server
                         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-                        print(host)
-                        print(host.decode('utf-8'))
                         s.connect((host.decode('utf-8'), 80))
 
                         header = header.replace(url, relative_url)
                         header = header.replace(b'localhost:8888', host)
-                        print(header)
                         
                         encoding = header.split(b'Accept-Encoding: ')[1].split(b'\r\n')[0]
                         header = header.replace(encoding, b'identity')                        
                         s.sendall(header)
-                        print('Header sent successfully')
-                        
 
                         response = s.recv(65565)
 
@@ -95,37 +92,26 @@ def proxy(expiry):
                         content_type = response_header.split(b'Content-Type: ')[1].split(b'\r\n')[0]
                         is_html = b'text/html' in content_type
 
-                        print(response)
-
                         try:
                             byte_content_length = response_header.split(b'Content-Length: ')[1].split(b'\r\n')[0]
                             content_length = int(byte_content_length.decode('utf-8'))
 
                             content = response.split(b'\r\n\r\n')[1]
                             while len(content) != content_length:
-                                print('Content length: {}, needed len: {}'.format(len(content), content_length))
                                 content += s.recv(65565)
 
                             response = response_header + b'\r\n\r\n' + content #this is the full response now
-
-                            print(response)
-                            
-                            
+      
                         except: #HTTP code 304 or Content-Length not specified
-                            print('HTTP code 304 or Content-Length not specified')
-
                             if b'200 OK' in response:
-
                                 while True:
                                     incoming = s.recv(65565)
                                     if len(incoming) == 0:
                                         break;
-                                    print (incoming)
-                                    print('Receieved incoming data from web server')
+                                   
                                     response += incoming
                             
                         finally:
-
                             if is_html:
                                 pre_body = response.split(b'<body')[0]
                                 post_body = response.split(b'<body')[1]
@@ -140,23 +126,20 @@ def proxy(expiry):
                                 fresh_time = bytes(fresh_time, encoding='utf8')
 
                                 if content_length:
-                                    
                                     content_length += 251
                                     response_body1 = response_body1.replace(b'Content-Length: ' +
                                                                             byte_content_length,
                                                                             b'Content-Length: ' +
                                                                             bytes(str(content_length),encoding='utf8'))
                                     
-                                
                                 fresh_response = response_body1 + body_tag + b'<p style="z-index:9999; position:fixed; top:20px; left:20px; \
                                 width:200px; height:100px; background-color:yellow; padding:10px; font-weight:bold;">FRESH VERSION \
                                 AT: ' + fresh_time + b'</p>' + response_body2
-                                
+
                                 connection.sendall(fresh_response)
                             else:
                                 connection.sendall(response)
-                                print('Sent response to client')
-                            
+                                
                             #Write the response to file
                             with open(filename, mode='wb') as file:
                                 if is_html:
@@ -164,19 +147,17 @@ def proxy(expiry):
                                     cache_time = bytes(cache_time, encoding='utf8')
 
                                     if content_length:
-                                        content_length += 12
                                         response_body1 = response_body1.replace(b'Content-Length: ' +
-                                                                                byte_content_length,
+                                                                                bytes(str(content_length),encoding='utf8'),
                                                                                 b'Content-Length: ' +
-                                                                                bytes(str(content_length),encoding='utf8'))
+                                                                                bytes(str(content_length + 12),encoding='utf8'))
                                     
                                     response = response_body1 + body_tag + b'<p style="z-index:9999; position:fixed; top:20px; left:20px; \
                                     width:200px; height:100px; background-color:yellow; padding:10px; font-weight:bold;">CACHED VERSION AS \
                                     OF: '+ cache_time + b'</p>' + response_body2
-                            
-                                print('Response: {}'.format(response))
+ 
+                                
                                 file.write(response)
-                                print('Response written successfully')
                                 
                             s.close()
 
@@ -185,15 +166,11 @@ def proxy(expiry):
                     #https://stackoverflow.com/questions/32792333/
                     #python-socket-module-connecting-to-an-http-proxy-then-performing-a-get-request
                     except socket.error as m:
-                       print (str(m))
-                       s.close()
-                       sys.exit(1)
-
-                
-                
+                        s.close()
+                        sys.exit(1)
                 
             else:
-                print('no data from', client_address)
+                pass
 
         finally:
             # Clean up the connection
